a9612b780251d793847c12b22416ff1791fb098b
Paulo Vieira <paulo1vieira.2004@gmail.com>
2025-03-20 14:56:05 +0100

    map

--- src/Pages/Process.tsx
+++ src/Pages/Process.tsx
@@ -1,7 +1,13 @@
+import Map from "../Components/Map"
+import { Container, Row } from "react-bootstrap"
 
 function Process() {
     return (
-        <div>Process</div>
+        <Container fluid className="d-flex justify-content-center align-items-center mb-5 mt-4">
+            <Row className="d-flex justify-content-center align-items-center w-100">
+                <Map></Map>
+            </Row>
+        </Container>
     )    
 }
 
