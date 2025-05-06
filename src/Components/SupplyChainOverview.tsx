e0380cf0e5ad331f68430d8ddb19cfa6d1ba9c55
Paulo Vieira <paulo1vieira.2004@gmail.com>
2025-05-06 19:57:31 +0200

    .

--- src/Components/SupplyChainOverview.tsx
+++ src/Components/SupplyChainOverview.tsx
@@ -1,5 +1,5 @@
 import { Container, Row, Col, Card } from "react-bootstrap";
-import { FaIndustry, FaTools, FaShip, FaSolarPanel } from "react-icons/fa";
+import { FaIndustry, FaTools, FaShip, FaSolarPanel, FaRecycle } from "react-icons/fa";
 
 const supplyChainData = [
     {
@@ -25,28 +25,35 @@ const supplyChainData = [
         location: "Netherlands",
         description: "Solar panels are installed with a 30-year lifespan and 80% power retention. Each panel is fully traceable from module to polysilicon origin via serial and QR code tracking.",
         icon: <FaSolarPanel size={50} />
+    },
+    {
+        title: "End-of-Life Recycling",
+        location: "Netherlands",
+        description: "Solarwatt is a participant in ZRN, the Dutch recycling network for solar panels. Through ZRN’s link with Stichting OPEN, Solarwatt helps improve how end-of-life solar panels are collected and recycled in the Netherlands.",
+        icon: <FaRecycle size={50} />
     }
 ];
 
 
+
 const SupplyChainOverview = () => {
     return (
-        <Container fluid className="py-5 px-5" style={{ backgroundColor: "#E6F4E6", minHeight: "90vh" }}>
+        <Container fluid className="py-5 px-0" style={{ backgroundColor: "#E6F4E6", minHeight: "90vh" }}>
             <Row className="text-center mb-4 text-primary p-4 mt-5">
                 <Col>
                     <h1 className="fw-bold">Supply Chain Overview</h1>
                     <p className="lead">A detailed look at each stage of our solar panel production and distribution.</p>
                 </Col>
             </Row>
-            <Row className="justify-content-center">
+            <Row className="justify-content-center px-0 gap-3">
                 {supplyChainData.map((stage, index) => (
-                    <Col key={index} md={6} lg={3} className="mb-4">
-                        <Card className="text-center border-0 shadow-sm p-3 rounded-4" style={{ backgroundColor: "#F4FAF4", minHeight: "100%" }}>
+                    <Col key={index} md={10} lg={2} className="mb-4 ">
+                        <Card className="text-center border-0 shadow-sm px- rounded-4" style={{ backgroundColor: "#F4FAF4", minHeight: "100%" }}>
                             <Card.Body>
                                 <div className="mb-3 text-primary">{stage.icon}</div>
                                 <Card.Title className="fw-bold pt-2">{stage.title}</Card.Title>
                                 <Card.Subtitle className="text-muted mb-2 pb-2">{stage.location}</Card.Subtitle>
-                                <Card.Text>{stage.description}</Card.Text>
+                                <Card.Text className="">{stage.description}</Card.Text>
                             </Card.Body>
                         </Card>
                     </Col>
